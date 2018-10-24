fdb217bb17ec1613fc0da35b79ec8e5978307d4d
AHAOAHA <ahaoaha_@outlook.com>
2018-10-24 18:01:18 +0800

    匿名管道

--- C/test/test.c
+++ C/test/test.c
@@ -1,16 +1,6 @@
-#include<stdio.h>
-#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
 int main()
 {
-  char* str = "hello world i am a boy\n";
-  char* argv[10] = {NULL};
-  int i = 0;
-  char* buf = " ";
-  char* p;
-  printf("%s\n",strtok(str," "));
-  while(p = strtok(NULL," "))
-  {
-    printf("%s\n",p);
-  }
   return 0;
 }
