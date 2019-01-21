27830abb5add5611dd93b1ac189116e3e1714e76
AHAOAHA <ahaoaha_@outlook.com>
2019-01-21 22:47:37 +0800

    模拟实现简单的shared_ptr

---
+++ C++/smart_ptr/shared_ptr/main.cc
@@ -0,0 +1,14 @@
+/*************************************************************************
+*File Name: main.cc
+*Author: AHAOAHA
+*mail: ahaoaha_@outlook.com
+*Created Time: 2019年01月21日 星期一 22时19分40秒
+ ************************************************************************/
+#include "shared_ptr.hpp"
+
+int main()
+{
+  Shared_Ptr<int> sp(new int(1));
+  
+  return 0;
+}
