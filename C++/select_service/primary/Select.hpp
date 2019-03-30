c35c9171811e360e14000a6aa2e2b3769f310174
AHAOAHA <ahaoaha_@outlook.com>
2019-03-30 15:12:57 +0800

    修改搬移fd的错误;处理select不阻塞的状况

--- C++/select_service/primary/Select.hpp
+++ C++/select_service/primary/Select.hpp
@@ -14,27 +14,39 @@
 #include <unistd.h>
 #include <iostream>
 
+#define FDSETSIZE sizeof(fd_set)*8
+
 using std::cout;
 using std::endl;
 
 namespace AHAOAHA {
-    void AddFdToVec(std::vector<int>& v, int &max_sock, const int &sock) {
+    bool AddFdToVec(std::vector<int>& v, int &max_sock, const int &sock) {
+        if(v.size() == FD_SETSIZE) {
+            close(sock);
+            return false;
+        }
         v.push_back(sock);
         for(const auto& e : v) {
             if(e > max_sock) {
                 max_sock = e;
             }
         }
+        return true;
     }
 
     void DelFdInVec(std::vector<int>& v, int &max_sock, const int &pos) {
-        cout << "DELFD : " << pos << endl;
        v.erase(v.begin()+pos);
        for(const auto& e : v) {
            if(e > max_sock) {
                max_sock = e;
            }
        }
+    }
+
+    bool MoveFd(std::vector<int>& v_source, std::vector<int>& v_obj, int &max_sock,const int& pos) {
+        int tmp = v_source[pos];    //将源fd保存
+        DelFdInVec(v_source, max_sock, pos);
+        return AddFdToVec(v_obj, max_sock, tmp);
     }
 
     void SetSet(std::vector<int>& v, fd_set &set) {
--- C++/select_service/primary/select.cc
+++ C++/select_service/primary/select.cc
@@ -16,6 +16,7 @@
 
 #define BUFSIZE 10240
 
+
 int main(int argc, char *argv[]) {
     if(argc < 2) {
         return -1;
@@ -30,61 +31,67 @@ int main(int argc, char *argv[]) {
 
     //创建并初始化socket 并且使套接字处于listen状态
     int sock = AHAOAHA::SetUpSocket(atoi(argv[1]));
-    AHAOAHA::AddFdToVec(fd_read, max_sock, sock);
+    if(AHAOAHA::AddFdToVec(fd_read, max_sock, sock) == false) {
+        cerr << "TOO MANY FD ..." << endl;
+        return -1;
+    }
 
     //将套接字添加进读文件描述符集合
     AHAOAHA::SetSet(fd_read, readfds);
     AHAOAHA::SetSet(fd_write, writefds);
+    int count = 0;
     //开始监听
     for(;;) {
-        int num = select(max_sock + 1, &readfds, &writefds, NULL, NULL);
+        cout << "select ---------" << endl;
+        struct timeval timeout = {3, 0};
+        int num = select(max_sock + 1, &readfds, &writefds, NULL, &timeout);
+        cout << "select ********* ret: "<< num  << " readfdvecsize: " << fd_read.size() << endl;
+        //出现错误情况直接修改位图并直接进行下次循环
         if(num < 0) {
             //fd_set清0 当readfds清0时 表示发生错误 TODO
-            continue;
+            AHAOAHA::SetSet(fd_read, readfds);
+            AHAOAHA::SetSet(fd_write, writefds);
         }
         else if(num == 0) {
             //重新设置需要监听的文件描述符
+            cout << "< TIME OUT ..." << endl;
             AHAOAHA::SetSet(fd_read, readfds);
             AHAOAHA::SetSet(fd_write, writefds);
-            continue;
         }
+
+        //正常接收
         else {  //事件发生 处理事件
             for(size_t i = 0; i < fd_read.size(); ++i) { //判断读事件
                 if(FD_ISSET(fd_read[i], &readfds)) {
                     //读事件发生
                     if(fd_read[i] == sock) { //套接字读事件
-                        cout << "SOCKET CONNECT ..." << endl;
                         struct sockaddr_in caddr;
                         socklen_t len = sizeof(caddr);
+
                         int clientfd = accept(fd_read[i], (struct sockaddr*)&caddr, &len);
-                        AHAOAHA::AddFdToVec(fd_read, max_sock, clientfd);
-                        continue;
+                        if(clientfd > 0) {
+                            if(AHAOAHA::AddFdToVec(fd_read, max_sock, clientfd) == false) {
+                                cerr << "TOO MANY FD ..." << endl;
+                            }
+                        }
                     }
                     else { //读事件发生在客户端套接字
-                        cout << "HTTP REQFD : " << i << endl;
-                        cout << "HTTP REQUEST ..." << endl;
                         char buffer[BUFSIZE] = {0};
                         int ret = recv(fd_read[i], buffer, BUFSIZE - 1, 0);
                         if(ret == 0) { //对端关闭链接
-                            close(fd_read[i]);
-                            AHAOAHA::DelFdInVec(fd_read, max_sock, i);
-                            //fd_read.erase(fd_read.begin()+i);
-                            continue;
+                            close(fd_read[i]);  //关闭套接字
+                            AHAOAHA::DelFdInVec(fd_read, max_sock, i);  //将该套接字从读位图中删除
                         }
                         else if(ret < 0) { //发生错误
-                            close(fd_read[i]);
-                            AHAOAHA::DelFdInVec(fd_read, max_sock, i);
-                            //fd_read.erase(fd_read.begin()+i);
-                            continue;
+                            close(fd_read[i]);  //关闭套接字
+                            AHAOAHA::DelFdInVec(fd_read, max_sock, i);  //将该套接字从读位图中删除
                         }
                         else { //正常接收
                             buffer[ret] = 0;
+                            cout << "*******************************************" << endl;
                             cout << buffer << endl;
-                            AHAOAHA::DelFdInVec(fd_read, max_sock, i);
-                            //fd_read.erase(fd_read.begin()+i);
-                            AHAOAHA::AddFdToVec(fd_write, max_sock, fd_read[i]);
-                            //fd_write.push_back(fd_read[i]);
-                            continue;
+                            cout << "*******************************************" << endl;
+                            AHAOAHA::MoveFd(fd_read, fd_write, max_sock, i);
                         }
                     }
                 } //读事件处理完毕
@@ -97,14 +104,30 @@ int main(int argc, char *argv[]) {
                 if(FD_ISSET(fd_write[i], &writefds)) {
                     //写事件发生
                     std::string resp = "HTTP/1.1 200 OK\r\n\r\n<html><body><h1>hello select!</h1></body></html>\r\n";
-                    write(fd_write[i], resp.c_str(), resp.size());  //无论写入正常与否 都是要关闭连接的
+                    send(fd_write[i], resp.c_str(), resp.size(), 0);  //无论写入正常与否 都是要关闭连接的
                     close(fd_write[i]);
                     AHAOAHA::DelFdInVec(fd_write, max_sock, i);
                 }
             }
             AHAOAHA::SetSet(fd_write, writefds);
         }
+
+        cout <<"count: "<<count<< " fd_read: ";
+        for(auto e : fd_read)
+        {
+            cout << e << " ";
+        }
+        cout << endl;
+        cout <<"count: " << count<< " fd_write: ";
+        for(auto e : fd_write)
+        {
+            cout << e << " ";
+        }
+        cout << endl;
+        count++;
     }
+
+    //意外退出，关闭所有文件描述符
     for(auto e : fd_read) {
         close(e);
     }
