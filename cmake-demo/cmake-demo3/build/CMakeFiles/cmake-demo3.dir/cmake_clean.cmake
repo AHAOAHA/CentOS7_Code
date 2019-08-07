bc222ea1b079c3109794208dadf7911a1d904131
AHAOAHA <ahaoaha_@outlook.com>
2019-08-07 15:44:57 +0800

    cmake include link target

---
+++ cmake-demo/cmake-demo3/build/CMakeFiles/cmake-demo3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-demo3.dir/main.cpp.o"
+  "cmake-demo3.pdb"
+  "cmake-demo3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake-demo3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-demo/helloworld-v2/hello/hello.h
@@ -0,0 +1,11 @@
+/*************************************************
+ * Copyright(C) 2016-2019. All right reserved.
+ * 
+ * Filename: hello.h
+ * Author: ahaoozhang
+ * Date: 2019-07-22 12:14:55 (星期一)
+ * Describe: 
+ *************************************************/
+#include <stdio.h>
+
+ void hello();
